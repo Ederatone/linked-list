d48256e3bbe2acded2eaa8f759f21a04b4be9e7b
Ederatone <vshkarbanw@ukr.net>
2025-06-04 15:02:52 +0300

    created basic List class

---
+++ list_module.py
@@ -0,0 +1,53 @@
+class List:
+    def __init__(self):
+        self.data = []
+
+    def length(self) -> int:
+        return len(self.data)
+
+    def append(self, element: str) -> None:
+        self.data.append(element)
+
+    def insert(self, element: str, index: int) -> None:
+        if index < 0 or index > len(self.data):
+            raise IndexError("Index out of bounds")
+        self.data.insert(index, element)
+
+    def delete(self, index: int) -> str:
+        if index < 0 or index >= len(self.data):
+            raise IndexError("Index out of bounds")
+        return self.data.pop(index)
+
+    def deleteAll(self, element: str) -> None:
+        self.data = [x for x in self.data if x != element]
+
+    def get(self, index: int) -> str:
+        if index < 0 or index >= len(self.data):
+            raise IndexError("Index out of bounds")
+        return self.data[index]
+
+    def clone(self):
+        new_list = List()
+        new_list.data = self.data.copy()
+        return new_list
+
+    def reverse(self) -> None:
+        self.data.reverse()
+
+    def findFirst(self, element: str) -> int:
+        try:
+            return self.data.index(element)
+        except ValueError:
+            return -1
+
+    def findLast(self, element: str) -> int:
+        for i in range(len(self.data) - 1, -1, -1):
+            if self.data[i] == element:
+                return i
+        return -1
+
+    def clear(self) -> None:
+        self.data = []
+
+    def extend(self, other: 'List') -> None:
+        self.data.extend(other.data.copy())
